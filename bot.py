bc014e335a7fe0e404dc054b0b401370a59ada2b
Floppat <turtleprogram33@gmai.com>
2025-01-18 17:38:12 +0200

    autoban for containing links

--- bot.py
+++ bot.py
@@ -23,4 +23,18 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(func=lambda message: True)
+def auto_ban(message):
+    chat_id = message.chat.id # сохранение id чата
+        # сохранение id и статуса пользователя, отправившего сообщение
+    user_id = message.from_user.id
+    user_status = bot.get_chat_member(chat_id, user_id).status 
+        # проверка пользователя
+    if user_status == 'administrator' or user_status == 'creator':
+        pass
+    else:
+        if 'https://' in message.text:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен за публикацию ссылки.")
+
 bot.infinity_polling(none_stop=True)
